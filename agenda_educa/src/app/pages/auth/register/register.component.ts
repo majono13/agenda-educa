42eb1d76039dbf069b90e349f63f4a553597a9b7
majono13 <mariaklmn@outlook.com>
2023-02-03 20:42:26 -0300

    Inicialização do desenvolvimento do componente de registro
    Foi inicializada o desenvolvimento do componente de registro. Também foram feitas alguma mudanças no componente de login para melhorar a resposividade.
    Mixins de animações adicionados.

--- agenda_educa/src/app/pages/auth/register/register.component.ts
+++ agenda_educa/src/app/pages/auth/register/register.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
+  //encapsulation: ViewEncapsulation,
 })
 export class RegisterComponent implements OnInit {
-  constructor() {}
+  constructor(private _location: Location) {}
 
   ngOnInit(): void {}
+
+  back() {
+    this._location.back();
+  }
 }
--- agenda_educa/src/app/shared/components/logo/logo.component.ts
+++ agenda_educa/src/app/shared/components/logo/logo.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
   templateUrl: './logo.component.html',
-  styleUrls: ['./logo.component.scss']
+  styleUrls: ['./logo.component.scss'],
 })
 export class LogoComponent implements OnInit {
+  @Input('_class') _class: string = 'logo';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
